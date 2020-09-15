2083c1a3b51e7cf4fac30ffe0179bcea4535f0da
Roy Zwambag <royzwambag@gmail.com>
2020-09-15 23:54:37 +0200

    Use textcontent for safety

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   currentChengyu = chengyu[Math.floor(Math.random()*chengyu.length)]
-  document.getElementById("first-hanzi").innerHTML = currentChengyu[0][0]
-  document.getElementById("second-hanzi").innerHTML = currentChengyu[0][1]
-  document.getElementById("third-hanzi").innerHTML = currentChengyu[0][2]
-  document.getElementById("fourth-hanzi").innerHTML = currentChengyu[0][3]
-  document.getElementById("pinyin").innerHTML = currentChengyu[1]
-  document.getElementById("meaning").innerHTML = currentChengyu[2]
+  document.getElementById("first-hanzi").textContent = currentChengyu[0][0]
+  document.getElementById("second-hanzi").textContent = currentChengyu[0][1]
+  document.getElementById("third-hanzi").textContent = currentChengyu[0][2]
+  document.getElementById("fourth-hanzi").textContent = currentChengyu[0][3]
+  document.getElementById("pinyin").textContent = currentChengyu[1]
+  document.getElementById("meaning").textContent = currentChengyu[2]
 }
